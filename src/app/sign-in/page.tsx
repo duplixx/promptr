c70765ff486900881b4649361111391223bae376
duplixx <90516956+duplixx@users.noreply.github.com>
2024-12-31 17:37:35 +0530

    minor checks

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -114,7 +114,7 @@ const SignIn = () => {
 
         <div className="mt-6 text-center">
           <p className="text-sm text-gray-600">
-            Don't have an account?{" "}
+            Don&apos;t have an account?{" "}
             <Link
               href="/sign-up"
               className="text-indigo-600 hover:text-indigo-500"
